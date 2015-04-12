24a7132719b2c12c5e15027dc947c0e9ca6de9b9
heymelon <jean.salmensuu@gmail.com>
2015-04-12 13:37:00 +0000

    Chefpage and more

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -17,17 +17,35 @@ class ChefsController < ApplicationController
       
   end
   
-  def index
-      
-  end
+
   
   def edit
-      
+    @chef = Chef.find(params[:id])
+     
   end
   
   def update
+    @chef = Chef.find(params[:id])
+      if @chef.update(chef_params)
+       flash[:success] = "Your profile has been updated"
+       redirect_to recipes_path #create chefpage
+      else
+        render 'edit'
+      end
       
   end
+  
+  def show
+   @chef = Chef.find(params[:id])
+   @recipes = @chef.recipes.paginate(page: params[:page], per_page: 4)
+  end
+  
+  def index
+    @chefs = Chef.paginate(page: params[:page], per_page: 4)
+  end
+  
+  
+  private
   
     def chef_params
         params.require(:chef).permit(:chefname, :email, :password)
